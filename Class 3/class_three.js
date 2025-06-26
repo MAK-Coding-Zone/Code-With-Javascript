ff154135622be566dc8688d80383162fd21e75a4
MuzammilAliKhan62 <164666108+MuzammilAliKhan62@users.noreply.github.com>
2025-06-26 18:01:47 +0500

    Create class_three.js

---
+++ Class 3/class_three.js
@@ -0,0 +1,16 @@
+// console.log("muzammil ali khan");
+var a = 10;
+// a=15; //reassign
+var a="muzammil";
+console.log(a);
+
+let b = 20;
+// b=25; //reassign
+let b="muzammil";
+console.log(b);
+
+const c = 30;
+// const c="muzammil";
+// c=56; // reassigning a constant will throw an error
+console.log(c);
+
